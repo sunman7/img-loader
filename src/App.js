59535c161372ebe6a9d88d54febfbbd3b0e1b401
sunman7 <473910527@qq.com>
2021-01-28 15:15:31 +0800

    添加router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Switch, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import History from "./pages/History";
+
+import React, {Suspense, lazy} from "react";
+import Loading from "./components/Loading";
+
+
+const Home = lazy(() => {
+    import("./pages/Home");
+});
+const History = lazy(() => {
+    import("./pages/History");
+});
+const About = lazy(() => {
+    import("./pages/About");
+});
 
 function App() {
     return (
         <div className="app">
             <Header/>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/about" exact component={About}/>
-                <Route path="/history" exact component={History}/>
-            </Switch>
+            <Suspense fallback={Loading}>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" exact component={About}/>
+                    <Route path="/history" exact component={History}/>
+                </Switch>
+            </Suspense>
             <Footer/>
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Logo from "../logo.svg";
+// import Logo from "../logo.svg";
 import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div>
-            <img src={Logo}/>
             <nav>
                 <Link to="/">首页</Link>
                 <Link to="/history">上传历史</Link>
