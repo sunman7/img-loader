8c7ac622797240b177e4163de2678ea637db715b
sunman7 <473910527@qq.com>
2021-02-21 13:47:59 +0800

    关于页面

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+ font-size: 40px;
+ display: flex;
+ margin: 0 auto;
+ flex-direction: column;
+ align-items: center;
+`;
 
 function About() {
     return (
         <>
-            <div>About</div>
+            <Div><span>作者是一个刚进入前端行业的菜鸡~</span>
+                <a href="">github</a>
+            </Div>
         </>
     );
 }
